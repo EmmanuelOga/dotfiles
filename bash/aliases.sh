b4094198305fe4c912739035db2d26ce7d5fbd65
Emmanuel Oga <emmanueloga@google.com>
2014-07-11 15:59:23 -0700

    cleanup

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,38 +1,22 @@
 alias ..="cd .."
-
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
+alias ack="ack-grep"
+alias be="bundle exec"
+alias bx="bundle exec"
 alias cd..="cd .."
 alias cdgems='cd `gem env gemdir`/gems'
-
 alias dir='dir --color=auto'
 alias egrep='egrep --color=auto'
 alias exes='find -type f -executable'
 alias fgrep='fgrep --color=auto'
 alias flattendirs="find -type f | xmv .; find -type d | escapepath | xargs rm -rf"
-
-alias ack="ack-grep"
+alias gpcb="git push origin \`git rev-parse --abbrev-ref HEAD\`"
 alias grep='grep --color=tty -d skip'
+alias hosts="gksu gvim /etc/hosts"
 alias ifind='find -type f | grep -i'
-
-alias l="tree -L 1 -C -h -F -p --dirsfirst -A"
 alias la='ls --group-directories-first --time-style=+"%Y-%m-%d %H:%M" --color=auto -F -la'
-alias ll="tree -L 1 -C -h -a -F -p --dirsfirst -A"
 alias ls='ls --group-directories-first --time-style=+"%Y-%m-%d %H:%M" --color=auto -F'
-alias vdir='vdir --color=auto'
-
-alias msp="mysql -uroot -proot"
-alias msa="mysqladmin -uroot -proot"
-alias mse="mysql -uroot -proot -B -e"
-
-alias reload='source ~/.bashrc'
-alias rhino='java -jar /usr/share/java/js.jar'
-alias srez="xrandr -s 0" # restore screen size to screen's best
-alias xmv='escapepath | xargs -I {} mv {}'
-
 alias rake="rake --trace"
-alias bx="bundle exec"
-alias vagrant="rvm 1.8.7 exec vagrant"
-
+alias rezet="xrandr -s 0 --orientation normal" # restore screen size to screen's best
 alias tpoff="synclient TouchpadOff=1"
 alias tpon="synclient TouchpadOff=0"
+alias xmv='escapepath | xargs -I {} mv {}'
